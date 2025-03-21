73c7dbaa2dd1d9ff991b13997289dd404130706b
helloItIsThomas <helloitisthomasm@gmail.com>
2025-03-21 18:44:05 -0400

    ~

--- src/main.js
+++ src/main.js
@@ -6,28 +6,30 @@ window.addEventListener("load", () => {
   const prevBtn = document.querySelector(".prev-btn");
   const nextBtn = document.querySelector(".next-btn");
 
+  const moveVal = 100 / items.length;
+
   prevBtn.addEventListener("click", () => {
     gsap.to(containerForItems, {
-      x: "+=33%",
+      x: `+=${moveVal}%`,
       duration: 0.5,
       ease: "power2.inOut",
       onComplete: () => {
         const lastItem = containerForItems.lastElementChild;
         containerForItems.prepend(lastItem);
-        gsap.set(containerForItems, { x: "-=33%" });
+        gsap.set(containerForItems, { x: `-=${moveVal}%` });
       },
     });
   });
 
   nextBtn.addEventListener("click", () => {
     gsap.to(containerForItems, {
-      x: "-=33%",
+      x: `-=${moveVal}%`,
       duration: 0.5,
       ease: "power2.inOut",
       onComplete: () => {
         const firstItem = containerForItems.querySelector(".item-container");
         containerForItems.appendChild(firstItem);
-        gsap.set(containerForItems, { x: "+=33%" });
+        gsap.set(containerForItems, { x: `+=${moveVal}%` });
       },
     });
   });
