c4db126c31fa7047c5bdc43de3651269d86e38e9
helloItIsThomas <helloitisthomasm@gmail.com>
2025-03-25 15:51:59 -0400

    progress

---
+++ data/someData.js
@@ -0,0 +1,34 @@
+export const someData = {
+  projects: [
+    {
+      id: 1,
+      name: "Bacon",
+      description: "Project 1 description",
+      image: "public/0.png",
+    },
+    {
+      id: 2,
+      name: "White",
+      description: "Project 2 description",
+      image: "public/1.png",
+    },
+    {
+      id: 3,
+      name: "Women",
+      description: "Project 3 description",
+      image: "public/2.png",
+    },
+    {
+      id: 4,
+      name: "Mario",
+      description: "Project 4 description",
+      image: "public/3.png",
+    },
+    {
+      id: 5,
+      name: "Gold",
+      description: "Project 5 description",
+      image: "public/4.png",
+    },
+  ],
+};
--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
-function populateFiveSlides(containerForItems) {
-  const lastItem = containerForItems.lastElementChild;
-  const firstItem = containerForItems.firstElementChild;
-  const lastClone = lastItem.cloneNode(true);
-  const firstClone = firstItem.cloneNode(true);
-  containerForItems.appendChild(firstClone);
-  containerForItems.prepend(lastClone);
+import { someData } from "../data/someData";
 
+function getNewProject() {
+  const numSlides = 5;
+  const numProjects = someData.projects.length;
+  const newProject = someData.projects[numSlides % numProjects];
+
+  return newProject;
+}
+
+function populateFiveSlides(containerForItems) {
+  // const lastItem = containerForItems.lastElementChild;
+  // const firstItem = containerForItems.firstElementChild;
+  // const lastClone = lastItem.cloneNode(true);
+  // const firstClone = firstItem.cloneNode(true);
   const items = document.querySelectorAll(".item");
-  return items;
+  const numSlides = 5;
+  const numProjects = someData.projects.length;
+
+  items.forEach((item, index) => {
+    const img = item.querySelector(".item-img");
+    img.src = someData.projects[index % numProjects].image;
+  });
 }
 
 window.addEventListener("load", () => {
@@ -15,25 +28,40 @@ window.addEventListener("load", () => {
   const nextBtn = document.querySelector(".next-btn");
   const containerForItems = document.querySelector(".containerForItems");
 
-  const items = populateFiveSlides(containerForItems);
+  populateFiveSlides(containerForItems);
+  const items = document.querySelectorAll(".item");
 
   const moveVal = 100 / items.length;
   const dur = 0.5;
 
+  let clickCounter = 0;
+
   prevBtn.addEventListener("click", () => {
+    const numSlides = 5;
+
     gsap.to(containerForItems, {
       x: `+=${moveVal}%`,
       duration: dur,
       ease: "power2.inOut",
       onComplete: () => {
-        const lastItem = containerForItems.lastElementChild;
-        const lastClone = lastItem.cloneNode(true);
+        const numProjects = someData.projects.length;
+
+        const setNewProjectToMe =
+          someData.projects[Math.abs(numSlides - clickCounter) % numProjects];
+
         const lastChild = containerForItems.lastElementChild;
+
+        lastChild.querySelector(".item-img").src = setNewProjectToMe.image;
+        console.log(setNewProjectToMe.name);
+        const lastClone = lastChild.cloneNode(true);
+
         containerForItems.prepend(lastClone);
         containerForItems.removeChild(lastChild);
         gsap.set(containerForItems, {
           x: `-=${moveVal}%`,
         });
+
+        clickCounter = (clickCounter + 1) % numSlides;
       },
     });
   });
