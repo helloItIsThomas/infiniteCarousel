aeb92bb8de3fc9fed61b5c7edf69a24df991d2a1
helloItIsThomas <helloitisthomasm@gmail.com>
2025-03-25 15:03:32 -0400

    ok i am close, now i just need to figure out which ones to clone if we have less than 5 unique projects

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
-window.addEventListener("load", () => {
-  const prevBtn = document.querySelector(".prev-btn");
-  const nextBtn = document.querySelector(".next-btn");
-  const containerForItems = document.querySelector(".containerForItems");
-
+function populateFiveSlides(containerForItems) {
   const lastItem = containerForItems.lastElementChild;
   const firstItem = containerForItems.firstElementChild;
   const lastClone = lastItem.cloneNode(true);
@@ -11,6 +7,15 @@ window.addEventListener("load", () => {
   containerForItems.prepend(lastClone);
 
   const items = document.querySelectorAll(".item");
+  return items;
+}
+
+window.addEventListener("load", () => {
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+  const containerForItems = document.querySelector(".containerForItems");
+
+  const items = populateFiveSlides(containerForItems);
 
   const moveVal = 100 / items.length;
   const dur = 0.5;
@@ -33,27 +38,6 @@ window.addEventListener("load", () => {
     });
   });
 });
-
-// tl.to(containerForItems, {
-// x: `+=${moveVal}%`,
-// duration: dur,
-// ease: "power2.inOut",
-// onComplete: () => {
-// console.log("done");
-// },
-// }).call(
-// () => {
-// console.log("midway");
-// const thirdItem = containerForItems.children[3];
-// const thirdClone = thirdItem.cloneNode(true);
-// containerForItems.replaceChild(
-// thirdClone,
-// containerForItems.firstElementChild
-// );
-// },
-// [],
-// "<50%"
-// );
 
 function makeNewTimeline(containerForItems, moveVal, dur) {
   const tl = gsap.timeline({
@@ -78,15 +62,6 @@ function makeNewTimeline(containerForItems, moveVal, dur) {
         thirdClone,
         containerForItems.firstElementChild
       );
-
-      // containerForItems.prepend(thirdClone);
-      // containerForItems.remove(thirdClone);
-      // const thirdItem = containerForItems.children[3];
-      // const thirdClone = thirdItem.cloneNode(true);
-      // containerForItems.replaceChild(
-      // thirdClone,
-      // containerForItems.firstElementChild
-      // );
     },
     [],
     "<50%"
